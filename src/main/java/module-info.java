10deb0a62de6395e7a9899d27cc79a17c05df736
M61ss <mattia.mas2003@gmail.com>
2024-04-09 19:51:44 +0200

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module org.example.cameraapi {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.bytedeco.javacv;
+    requires org.bytedeco.opencv;
 
 
     opens org.example.cameraapi to javafx.fxml;
