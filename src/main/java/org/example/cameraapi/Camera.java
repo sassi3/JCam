e061bd8595b00ed553ee4f24f213f3988f9396d2
Mattia Massarenti <mattia.mas2003@gmail.com>
2024-04-10 13:57:05 +0200

    Adding some other webcam solution (not working at the moment)

--- src/main/java/org/example/cameraapi/Camera.java
+++ src/main/java/org/example/cameraapi/Camera.java
@@ -1,13 +1,24 @@
 package org.example.cameraapi;
 
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamPanel;
+import com.github.sarxos.webcam.WebcamResolution;
+import com.github.sarxos.webcam.WebcamUtils;
+import com.github.sarxos.webcam.util.ImageUtils;
 import org.bytedeco.javacv.*;
 import org.bytedeco.javacv.FrameGrabber.Exception;
 import org.bytedeco.opencv.global.opencv_imgcodecs;
 import org.bytedeco.opencv.opencv_core.IplImage;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Camera {
 
-    public static void click() throws Exception {
+    public static void clickOpenCV() throws Exception {
         FrameGrabber grabber = new OpenCVFrameGrabber(0);
         grabber.start();
         Frame frame = grabber.grab();
@@ -20,7 +31,43 @@ public class Camera {
         canvas.showImage(frame);
     }
 
-    public static void main(String[] args) throws Exception {
-        Camera.click();
+    public static void clickWebcamCapture() throws IOException {
+        Webcam webcam = Webcam.getDefault();
+        webcam.open();
+
+        BufferedImage image = webcam.getImage();
+
+        ImageIO.write(image, ImageUtils.FORMAT_JPG, new File("selfie.jpg"));
+    }
+
+    public static void clickWebcamUtils() throws Exception {
+        Webcam webcam = Webcam.getDefault();
+        webcam.open();
+
+        WebcamUtils.capture(webcam, "selfie.jpg");
+    }
+
+    public static void clickWebcamShow() throws Exception {
+        Webcam webcam = Webcam.getDefault();
+        webcam.setViewSize(WebcamResolution.VGA.getSize());
+
+        WebcamPanel panel = new WebcamPanel(webcam);
+        panel.setImageSizeDisplayed(true);
+
+        JFrame window = new JFrame("Webcam");
+        window.add(panel);
+        window.setResizable(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.pack();
+        window.setVisible(true);
+    }
+
+    public static void main(String[] args) throws IOException {
+        // Now this is the only working
+        Camera.clickOpenCV();
+        // These are bruh and slow
+        // Camera.clickWebcamCapture();
+        // Camera.clickWebcamUtils();
+        // Camera.clickWebcamShow();
     }
 }
