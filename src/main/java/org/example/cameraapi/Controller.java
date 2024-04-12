70425b2fc732d36e609d992736fb0ece6483ac8a
M61ss <mattia.mas2003@gmail.com>
2024-04-12 20:06:41 +0200

    Cleanup and reorganized code
    Created constructors for Controller and Camera and getters and setters for Camera. Then some general changes to get the code easier. It is expected to remove the controller constructor... I think it is not a so good idea

--- src/main/java/org/example/cameraapi/Controller.java
+++ src/main/java/org/example/cameraapi/Controller.java
@@ -1,39 +1,33 @@
 package org.example.cameraapi;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 
 import javafx.scene.canvas.Canvas;
 import javafx.animation.AnimationTimer;
 import javafx.scene.image.Image;
 import org.bytedeco.javacv.FrameGrabber;
 import org.bytedeco.javacv.JavaFXFrameConverter;
-import org.bytedeco.javacv.OpenCVFrameGrabber;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
 import org.opencv.imgcodecs.Imgcodecs;
 import javafx.fxml.FXML;
 
 public class Controller  {
-    @FXML private Canvas camera;
-    AnimationTimer timer;
-    Camera cam;
-
-    {
-        try {
-            cam = new Camera(new JavaFXFrameConverter() );
-        } catch (FrameGrabber.Exception e) {
-            throw new RuntimeException(e);
-        }
+    @FXML private Canvas camera_canvas;
+    private final Camera camera;
+    
+    public Controller() throws Exception {
+        camera = new Camera();
+        camera.start();
+        initializeTimer();
     }
 
-
     public void initializeTimer() {
-        timer = new AnimationTimer() {
+        AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
                 try {
-                    Camera.showWebcam(camera,cam.grabber,cam.converter);
+                    Camera.showWebcam(camera_canvas, camera.getGrabber(), camera.getConverter());
                 } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
@@ -42,16 +36,10 @@ public class Controller  {
         timer.start();
     }
 
-    @FXML
-    public void handleStart() throws Exception {
-        cam.grabber.start();
-        initializeTimer();
-    }
-
-    public static Image mat2Image(Mat mat) {
+    // Unused mat2Image converter, but maybe useful
+    /* public static Image mat2Image(Mat mat) {
         MatOfByte buffer = new MatOfByte();
         Imgcodecs.imencode(".png", mat, buffer);
         return new Image(new ByteArrayInputStream(buffer.toArray()));
-    }
-
+    } */
 }
