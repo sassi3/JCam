52a734ee75be768b64476f0a47d63d45e54856a8
Manuel Sassi <129498641+sassi3@users.noreply.github.com>
2024-04-12 16:03:28 +0200

    create app icon

--- src/main/java/org/example/cameraapi/App.java
+++ src/main/java/org/example/cameraapi/App.java
@@ -3,19 +3,21 @@ package org.example.cameraapi;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("camera.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("Icons/CameraIcon.png"))));
         stage.setTitle("Camera");
         stage.setScene(scene);
         stage.setResizable(false);
-
         stage.show();
     }
 
