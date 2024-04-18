6d9bdabb00c91fcc4ca97f1b45e8ec5ac1f99285
M61ss <mattia.mas2003@gmail.com>
2024-04-18 08:31:53 +0200

    Some refactoring operations

--- src/main/java/org/example/cameraapi/Controller.java
+++ src/main/java/org/example/cameraapi/Controller.java
@@ -18,23 +18,26 @@ public class Controller  {
     private AnimationTimer timer;
     private final Camera camera;
     @FXML private Canvas cameraCanvas;
-    @FXML private Image rawPicture;        // I think that it is a good practice to keep a copy of original data
+    @FXML private Image rawPicture;
     @FXML private Image currentPicture;
     @FXML private ImageView outputPicture;
     @FXML private WritableImage pictureToWrite;
     @FXML private Button captureButton;
-    private boolean outputChecker = true; // assures that the transform gets applied on output_picture only once
+    private boolean outputChecker;
+
     // By default, the camera preview is shown on program startup
-    public Controller() throws FrameGrabber.Exception {
+    public Controller() {
         camera = new Camera();
+        outputChecker = true;   // assures that the transform gets applied on output_picture only once
         initializeTimer();
     }
 
+    // -------------- DISARMER --------------
     public void disableInterface(){
         captureButton.disarm();
     }
 
-    // --------------- FXML ---------------
+    // --------------- FXMLs ---------------
     @FXML
     private void webcamStop() throws FrameGrabber.Exception {
         camera.stop();
@@ -49,33 +52,31 @@ public class Controller  {
 
     @FXML
     private void picturePreview() {
-        output_picture.setImage(raw_picture);
-        output_picture.setPreserveRatio(true);
+        outputPicture.setImage(rawPicture);
+        outputPicture.setPreserveRatio(true);
     }
 
     @FXML
     private void takePicture() throws FrameGrabber.Exception {
+        // ? Are you sure ?
         if (outputChecker) {
             outputChecker = false;
-            output_picture.getTransforms().add(new Affine(-1,0,output_picture.getFitWidth(),0,1,0));
-            // flips what's displayed by the image view around the y axis
-            // and then translates it right (through the x axis) by the width of the image view itself
+            outputPicture.getTransforms().add(new Affine(-1,0,outputPicture.getFitWidth(),0,1,0));
+            // flips what's displayed by the image view around the y-axis
+            // and then translates it right (through the x-axis) by the width of the image view itself
         }
-        Frame snap = camera.getGrabber().grab();
-        rawPicture = camera.getConverter().convert(snap);
-        this.picturePreview();
+        rawPicture = camera.getConverter().convert(camera.getGrabber().grab());
+        picturePreview();
     }
 
-    // ----------- CANVAS PRINTERS -----------
-    private void printFrame(Canvas canvas, FrameGrabber grabber, JavaFXFrameConverter converter) throws Exception {
-        GraphicsContext graphics2D = canvas.getGraphicsContext2D();
+    // --------- UNIVERSAL CANVAS PRINTERS ---------
+    private void printWebcamFrame(Canvas canvas, FrameGrabber grabber, JavaFXFrameConverter converter) throws Exception {
         Image img = converter.convert(grabber.grab());
-        graphics2D.drawImage(img, 0, 0);
+        canvas.getGraphicsContext2D().drawImage(img, 0, 0, canvas.getWidth(), canvas.getHeight());
     }
 
     private void printImg(Canvas canvas, Image img)  {
-        GraphicsContext graphics2D = canvas.getGraphicsContext2D();
-        graphics2D.drawImage(img, 0, 0);
+        canvas.getGraphicsContext2D().drawImage(img, 0, 0);
     }
 
     // -------------- TIMER --------------
@@ -88,7 +89,8 @@ public class Controller  {
                         disableInterface();
                         printImg(cameraCanvas, new Image(Objects.requireNonNull(getClass().getResourceAsStream("Icons/ErrImg.png"))));
                     } else {
-                        printFrame(cameraCanvas, camera.getGrabber(), camera.getConverter());
+                        printWebcamFrame(cameraCanvas, camera.getGrabber(), camera.getConverter());
+                        Effects.imgFlipper(cameraCanvas.getGraphicsContext2D());
                     }
                 } catch (Exception e) {
                     throw new RuntimeException(e);
