c085659f705027d7880f0a690b734f298b6c4f43
Manuel Sassi <129498641+sassi3@users.noreply.github.com>
2024-05-05 14:26:41 +0200

    Add basic method to editor class

--- src/main/java/org/cameraapi/controller/EditorController.java
+++ src/main/java/org/cameraapi/controller/EditorController.java
@@ -1,10 +1,30 @@
 package org.cameraapi.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class EditorController {
+    @FXML private ImageView picturePreview;
+    private Image picture;
     @FXML
     public void initialize() {
+        setPicturePreview(picture);
+    }
 
+    public ImageView getPicturePreview() {
+        return picturePreview;
+    }
+
+    public Image getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Image picture) {
+        this.picture = picture;
+    }
+
+    public void setPicturePreview(Image picture) {
+        picturePreview.setImage(picture);
     }
 }
