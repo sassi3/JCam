3be99619c53278e54a727af6f07e470b2b369051
Mattia Massarenti <mattia.mas2003@gmail.com>
2024-05-14 22:29:31 +0200

    Refactoring

--- src/main/java/org/cameraapi/common/WebcamListener.java
+++ src/main/java/org/cameraapi/common/WebcamListener.java
@@ -3,13 +3,13 @@ package org.cameraapi.common;
 import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamDiscoveryEvent;
 import com.github.sarxos.webcam.WebcamDiscoveryListener;
-import org.cameraapi.WebcamController;
+import org.cameraapi.HomeController;
 
 public class WebcamListener implements WebcamDiscoveryListener {
     public WebcamListener() {
         for (Webcam webcam : Webcam.getWebcams()) {
             System.out.println("Webcam detected: " + webcam.getName());
-            WebcamController.getWebcams().addLast(webcam);
+            HomeController.getWebcams().addLast(webcam);
         }
         Webcam.addDiscoveryListener(this);
         System.out.println("Webcam added. Listening for events...");
@@ -18,12 +18,12 @@ public class WebcamListener implements WebcamDiscoveryListener {
     @Override
     public void webcamFound(WebcamDiscoveryEvent webcamDiscoveryEvent) {
         System.out.println("Webcam connected: " + webcamDiscoveryEvent.getWebcam().getName());
-        WebcamController.getWebcams().addLast(webcamDiscoveryEvent.getWebcam());
+        HomeController.getWebcams().addLast(webcamDiscoveryEvent.getWebcam());
     }
 
     @Override
     public void webcamGone(WebcamDiscoveryEvent webcamDiscoveryEvent) {
         System.out.println("Webcam disconnected: " + webcamDiscoveryEvent.getWebcam().getName());
-        WebcamController.getWebcams().remove(webcamDiscoveryEvent.getWebcam());
+        HomeController.getWebcams().remove(webcamDiscoveryEvent.getWebcam());
     }
 }
