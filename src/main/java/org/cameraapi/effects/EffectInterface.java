54492dcb3ed19f2c2f31298d2f47f5b317783449
M61ss <mattia.mas2003@gmail.com>
2024-05-14 19:59:58 +0200

    Delete EffectInterface.java

--- src/main/java/org/cameraapi/effects/EffectInterface.java
+++
@@ -1,9 +0,0 @@
-package org.cameraapi.effects;
-
-public interface EffectInterface {
-    void enable();
-    void disable();
-    boolean isEnabled();
-    void apply();
-    boolean isApplied();
-}
