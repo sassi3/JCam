dd4216d2256a14f0aa79d59224667c27a0141837
Manuel Sassi <129498641+sassi3@users.noreply.github.com>
2024-05-21 15:00:50 +0200

    fix freeze problem
    FrameShowThread class now implements runnable instead of extending Thread.

--- src/main/java/org/cameraapi/common/FrameShowThread.java
+++ src/main/java/org/cameraapi/common/FrameShowThread.java
@@ -8,10 +8,14 @@ import javafx.scene.image.ImageView;
 import org.cameraapi.effects.Flip;
 import org.cameraapi.model.WebcamUtils;
 
-public class FrameShowThread extends Thread {
+import static java.lang.Thread.currentThread;
+import static java.lang.Thread.interrupted;
+
+public class FrameShowThread implements Runnable {
     private final ChoiceBox<Webcam> webcamList;
     private Webcam activeWebcam;
     private final ImageView webcamDisplay;
+    private Thread frameShowThread;
 
     public FrameShowThread(ChoiceBox<Webcam> webcamList, Webcam activeWebcam, ImageView webcamDisplay) {
         this.webcamList = webcamList;
@@ -41,19 +45,22 @@ public class FrameShowThread extends Thread {
     }
 
     public void startShowingFrame() {
-        if (!isAlive()) {
-            start();
+        frameShowThread = new Thread(this);
+        frameShowThread.setDaemon(true);
+        frameShowThread.setName("Webcam Frame Showing-Thread");
+        if (!frameShowThread.isAlive()) {
+            frameShowThread.start();
         }
-        if (!isAlive()) {
+        if (!frameShowThread.isAlive()) {
             throw new IllegalThreadStateException("Failed to start showing frames.");
         }
     }
 
     public void stopShowingFrame() throws InterruptedException {
-        if (isAlive()) {
-            interrupt();
-            join();
-            if (isAlive()) {
+        if (frameShowThread.isAlive()) {
+            frameShowThread.interrupt();
+            frameShowThread.join();
+            if (frameShowThread.isAlive()) {
                 throw new IllegalThreadStateException("Failed to stop frameShowThread.");
             }
         } else {
