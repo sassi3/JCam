7fcb94932a8d26dd7b40eabf181e8ef93fa00cc2
M61ss <mattia.mas2003@gmail.com>
2024-05-28 19:30:39 +0200

    Created slide effect

--- src/main/java/org/cameraapi/HomeController.java
+++ src/main/java/org/cameraapi/HomeController.java
@@ -6,12 +6,17 @@ import java.util.Objects;
 import java.util.Optional;
 import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamMotionDetector;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.collections.ListChangeListener;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import org.cameraapi.common.AlertWindows;
 import org.cameraapi.common.FrameShowThread;
 import org.cameraapi.common.WebcamListener;
@@ -208,8 +213,19 @@ public class HomeController {
         controller.initCanvas(capture);
         controller.initLiveEffects(liveEffects.get(Flip.class).isApplied());
 
+        Scene scene = mainPane.getScene();
+        root.translateXProperty().set(scene.getWidth());
         ScreenController.addScreen("editor", root);
         ScreenController.activate("editor");
+
+        Timeline timeline = new Timeline();
+        KeyValue kv = new KeyValue(root.translateXProperty(), 0, Interpolator.EASE_IN);
+        KeyFrame kf = new KeyFrame(Duration.seconds(0.4), kv);
+        timeline.getKeyFrames().add(kf);
+        timeline.setOnFinished(t -> {
+            mainPane.getChildren().remove(mainPane);
+        });
+        timeline.play();
     }
 
     @FXML
