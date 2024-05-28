1faa96c301841c3a64cb99bef2978394859b7e6b
Manuel Sassi <129498641+sassi3@users.noreply.github.com>
2024-05-28 23:14:26 +0200

    Update SaveDialogController.java
    Added access methods to textfields

--- src/main/java/org/cameraapi/SaveDialogController.java
+++ src/main/java/org/cameraapi/SaveDialogController.java
@@ -1,17 +1,43 @@
 package org.cameraapi;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.lang.annotation.Target;
+import java.nio.file.Path;
 
 public class SaveDialogController {
 
     @FXML
+    TextField selectDirTxtField;
+    @FXML
+    TextField fileNameTxtField;
+    @FXML
     ImageView preview;
 
-    public void save(){
+    private String fileName;
+    private String dir;
 
+    public void initialize() {
+        fileNameTxtField.setEditable(true);
+        fileNameTxtField.textProperty().addListener((observable, oldValue, newValue) -> {
+           fileName = newValue;
+        });
+        selectDirTxtField.textProperty().addListener((observable, oldValue, newValue) -> {
+            dir = newValue;
+        });
     }
+
+    public void save(){
+        
+    }
+
+
+
 
     public void initPreview(Image image){
         preview.setImage(image);
