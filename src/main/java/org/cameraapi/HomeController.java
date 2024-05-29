e52875f349ceb803dbbe1c2d6d99772c77c9041c
Manuel Sassi <129498641+sassi3@users.noreply.github.com>
2024-05-29 14:53:51 +0200

    Update HomeController.java
    Now the stabilityTray radio button is always selected if cam is frozen

--- src/main/java/org/cameraapi/HomeController.java
+++ src/main/java/org/cameraapi/HomeController.java
@@ -187,7 +187,12 @@ public class HomeController {
         liveEffects.get(Freeze.class).toggle(webcamDisplay);
         if (liveEffects.get(Freeze.class).isApplied()) {
             Freeze.freeze(frameShowThread);
+            stabilityTrayThread.interrupt();
+            stabilityTray.setSelected(true);
         } else {
+            stabilityTray.setSelected(false);
+            initStabilityTrayThread();
+            stabilityTrayThread.start();
             frameShowThread.startShowingFrame();
         }
         freezeToggleButton.setText(freezeToggleButton.isSelected() ? "Unfreeze" : "Freeze");
