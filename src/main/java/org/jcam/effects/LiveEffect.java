ac80cb30b9a56950228a9987109c58d3c2c2f5eb
M61ss <mattia.mas2003@gmail.com>
2024-06-16 12:19:59 +0200

    Update LiveEffect.java
    Since apply() method should not be used by LiveEffects, it throws an exception by default

--- src/main/java/org/jcam/effects/LiveEffect.java
+++ src/main/java/org/jcam/effects/LiveEffect.java
@@ -4,4 +4,9 @@ public abstract class LiveEffect extends Effect {
     public LiveEffect() {
         super();
     }
+
+    @Override
+    public void apply() {
+        throw new IllegalCallerException("This method is unsupported by " + this.getClass().getName());
+    }
 }
